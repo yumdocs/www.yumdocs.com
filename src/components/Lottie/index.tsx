c9c6bd0d1123b1fe7e82bf0e5612110bff698bec
jlchereau <jlchereau@msn.com>
2022-10-31 12:22:54 +0100

    Added more config options

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 // import clsx from 'clsx';
-import lottie from 'lottie-web';
+import {default as lottie, RendererType} from 'lottie-web';
 
 import styles from './styles.module.css';
 
 interface Props {
+    autoplay: boolean;
+    loop: boolean;
     path: string;
+    renderer: RendererType;
 }
 
-export default function Lottie({path}: Props) {
+export default function Lottie({autoplay = true, loop = true, path, renderer = 'svg'}: Props) {
     const lottieElement = React.useRef<HTMLDivElement>();
     React.useEffect(() => {
         lottie.loadAnimation({
             container: lottieElement.current, // the dom element that will contain the animation
-            renderer: 'svg',
-            loop: true,
-            autoplay: true,
+            renderer,
+            loop,
+            autoplay,
             path // the path to the animation json
         });
     });
