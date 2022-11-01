c3735f4e374c3d100903a382d9772824c1815d3b
jlchereau <jlchereau@msn.com>
2022-11-01 20:45:06 +0100

    Switched for 5min tutorials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
                   <Link
                     className="button button--secondary button--lg"
                     to="/docs/intro">
-                      Yumdocs Tutorial - 5min ⏱️
+                      ⏱️ 5min Tutorials
                   </Link>
                 </div>
               </div>
